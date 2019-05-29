2bbb201c6a57ba9bd086c04a13c5cba94c77b8c1
Paul Robertson <t.paulrobertson@gmail.com>
2019-05-29 15:55:57 -0400

    better semver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='statsd-telegraf',
-    version='3.2.1-1',
+    version='3.2.1.1',
     description='A fork of pystatsd package with Telegraf-compatible tag support.',
     long_description=open('README.rst').read(),
     author='Bernardas Ališauskas',
